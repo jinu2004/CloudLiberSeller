adad97e639d0aa7d84a44531d9d1c3f4dc4eb4e0
jinu2004 <jinujs53@gmail.com>
2023-11-29 13:02:23 +0530

    committed by Jinu

---
+++ src/main/kotlin/screens/SplashScreen.kt
@@ -0,0 +1,11 @@
+package screens
+
+import androidx.compose.runtime.Composable
+import navcontroller.NavController
+
+class SplashScreen(navController: NavController) {
+    @Composable
+    fun View(){
+
+    }
+}
