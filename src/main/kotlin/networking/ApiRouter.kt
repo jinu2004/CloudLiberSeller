5b469335ada9f7b8ce6bc6842d744e8d4db89769
jinu2004 <jinujs53@gmail.com>
2023-12-01 21:54:30 +0530

    changed to cloudliber firebase

--- src/main/kotlin/networking/ApiRouter.kt
+++ src/main/kotlin/networking/ApiRouter.kt
@@ -9,8 +9,8 @@ object ApiRouter {
 
 
 
-    const val APPID = "1:204239095350:android:af5391c0aecd170782fa97"
-    const val BUCKETNAME = "cloudlibre-database.appspot.com"
+    const val APPID = "1:470410615710:web:70368a9d1ff71a4171148f"
+    const val BUCKETNAME = "project-clouliber.appspot.com"
     const val FIREBASE_STORAGE = "https://firebasestorage.googleapis.com/v0/b/$BUCKETNAME/o?uploadType=media&name="
 
 //    https://firebasestorage.googleapis.com/v0/b/project-clouliber.appspot.com/o/coverimages%2F8aadee2eafece86269e20d025bab490e.jpg?alt=media&token=b86f091e-9375-4892-9231-012d6dfa6529
